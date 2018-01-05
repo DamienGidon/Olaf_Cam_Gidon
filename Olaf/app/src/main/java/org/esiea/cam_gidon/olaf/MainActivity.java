8802ebed0cbe65191e6a14ce820ed7e66e7ba9a7
Thibault Cam <thibault.cam1@yahoo.fr>
2018-01-05 15:23:02 +0100

    la

--- Olaf/app/src/main/java/org/esiea/cam_gidon/olaf/MainActivity.java
+++ Olaf/app/src/main/java/org/esiea/cam_gidon/olaf/MainActivity.java
@@ -1,9 +1,14 @@
 package org.esiea.cam_gidon.olaf;
 
+import android.app.Notification;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Toast;
+
 
 /**
  * Created by thiba on 05/01/2018.
@@ -11,9 +16,22 @@ import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void ToastCreator(View v){
+        Toast.makeText(getApplicationContext(),"Test", Toast.LENGTH_LONG).show();
+        //Toast.makeText(getApplicationContext(),getString(R.string.msg), Toast.LENGTH_LONG).show();
+    }
+
+    public void NotifCreator(View v){
+        //Notification notif
+    }
+
+    public void DialogCreator(View v){
+        //R.layout.activity_dialog
     }
 }
 
